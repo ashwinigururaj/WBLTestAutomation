2da2b3993a69d8a543e43a02b2fb7b5cbad4f2a6
whiteboxlearning <whiteboxqa@gmail.com>
2015-08-08 19:03:27 -0700

    Framework Initial

--- Framework/src/com/wbl/utils/PageDriver.java
+++ Framework/src/com/wbl/utils/PageDriver.java
@@ -1,7 +1,21 @@
 package com.wbl.utils;
 
+import org.apache.commons.logging.Log;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by svelupula on 8/8/2015.
  */
 public class PageDriver {
+
+    private final Configuration _configuration;
+    private final Browsers _browser;
+    private WebDriver _webDriver;
+    private String _mainWindowHandler;
+
+    public PageDriver(Configuration configuration) {
+        _configuration = configuration;
+        _browser = _configuration.Browser;
+    }
+
 }
